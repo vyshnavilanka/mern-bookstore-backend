55a2ebcd1994b84a4befe60031ecff9dd3ef02bd
vyshnavilanka <vaishulanka007@gmail.com>
2024-08-15 22:21:08 +0530

    commited

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import './db.js'
 import {AdminRouter} from './routes/auth.js'
@@ -9,34 +10,16 @@ import {Book} from './models/Book.js'
 import {Student } from './models/Student.js'
 import {Admin} from './models/Admin.js'
 
-dotenv.config();
 const app =express()
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://book-store-frontend1-03yo.onrender.com"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
 app.use(express.json())
 app.use(cors({
-    origin:['*'],
+    origin:['http://localhost:5173'],
     credentials: true
+ 
+
 }))
 app.use(cookieParser())
+
 dotenv.config()
 app.use('/auth',AdminRouter)
 app.use('/student',studentRouter)
@@ -50,13 +33,12 @@ app.get('/dashboard',async (req,res) =>{
         return res.json({ok:true,student,book,admin})
 
     } catch(err){
-        return res.status(500).json({ error: 'Internal Server Error', details: err.message });
+        return res.json(err)
 
     }
 
 })
 
-const port = process.env.PORT || 3000; // Default to port 3000 if not specified
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(process.env.PORT,() =>{
+    console.log("Serve is Running");
+} )
